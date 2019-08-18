69a48f868a87b98bb5382a67b5e10120732437af
DESKTOP-9L4KOLL\K <kore207@naver.com>
2019-08-18 22:36:20 +0900

    Calcurator

---
+++ C#/HelloCSharpWin/HelloCSharpWin/Calculator.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace HelloCSharpWin
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HelloLable = new System.Windows.Forms.Label();
+            this.Sum1 = new System.Windows.Forms.TextBox();
+            this.Sum2 = new System.Windows.Forms.TextBox();
+            this.SumNumbers = new System.Windows.Forms.Button();
+            this.SumResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HelloLable
+            // 
+            this.HelloLable.AutoSize = true;
+            this.HelloLable.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.HelloLable.Location = new System.Drawing.Point(29, 34);
+            this.HelloLable.Name = "HelloLable";
+            this.HelloLable.Size = new System.Drawing.Size(105, 12);
+            this.HelloLable.TabIndex = 0;
+            this.HelloLable.Text = "여기를 클릭하세요";
+            this.HelloLable.Click += new System.EventHandler(this.HelloLable_Click);
+            // 
+            // Sum1
+            // 
+            this.Sum1.Location = new System.Drawing.Point(15, 72);
+            this.Sum1.Name = "Sum1";
+            this.Sum1.Size = new System.Drawing.Size(100, 21);
+            this.Sum1.TabIndex = 1;
+            // 
+            // Sum2
+            // 
+            this.Sum2.Location = new System.Drawing.Point(137, 72);
+            this.Sum2.Name = "Sum2";
+            this.Sum2.Size = new System.Drawing.Size(100, 21);
+            this.Sum2.TabIndex = 2;
+            // 
+            // SumNumbers
+            // 
+            this.SumNumbers.Location = new System.Drawing.Point(243, 72);
+            this.SumNumbers.Name = "SumNumbers";
+            this.SumNumbers.Size = new System.Drawing.Size(75, 23);
+            this.SumNumbers.TabIndex = 3;
+            this.SumNumbers.Text = "=";
+            this.SumNumbers.UseVisualStyleBackColor = true;
+            this.SumNumbers.Click += new System.EventHandler(this.SumNumbers_Click);
+            // 
+            // SumResult
+            // 
+            this.SumResult.Location = new System.Drawing.Point(325, 73);
+            this.SumResult.Name = "SumResult";
+            this.SumResult.Size = new System.Drawing.Size(100, 21);
+            this.SumResult.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "+";
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 377);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SumResult);
+            this.Controls.Add(this.SumNumbers);
+            this.Controls.Add(this.Sum2);
+            this.Controls.Add(this.Sum1);
+            this.Controls.Add(this.HelloLable);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HelloLable;
+        private System.Windows.Forms.TextBox Sum1;
+        private System.Windows.Forms.TextBox Sum2;
+        private System.Windows.Forms.Button SumNumbers;
+        private System.Windows.Forms.TextBox SumResult;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ C#/HelloCSharpWin/HelloCSharpWin/Calculator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HelloCSharpWin
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloLable_Click(object sender, EventArgs e)
+        {
+            HelloLable.Text = "Hello C#";
+
+            int num1 = 1 ;
+            int num2 = 2 ;
+
+            int sum = num1 + num2 ;
+            HelloLable.Text = sum.ToString() ;
+        }
+
+        private void SumNumbers_Click(object sender, EventArgs e)
+        {
+            int number1 = 0;
+            int number2 = 0;
+
+
+            if (String.IsNullOrWhiteSpace(Sum1.Text))
+            {
+                MessageBox.Show("Sum1에 숫자를 입력해주세요.");
+                Sum1.SelectAll();
+                Sum1.Focus();
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(Sum2.Text))
+            {
+                MessageBox.Show("Sum2에 숫자를 입력해주세요.");
+                Sum2.SelectAll();
+                Sum2.Focus();
+                return;
+            }
+
+            if (int.TryParse(Sum1.Text, out number1) == false) //문자를 숫자로 바꾼다. (out 인자를 통해 첫번째 매개변수의 값을 받아온다 )
+            {
+                MessageBox.Show("Sum1에 문자가 들어왔습니다. 숫자를 입력해주세요 .");
+                Sum1.SelectAll();
+                Sum1.Focus();
+                return;
+            }
+
+            if (int.TryParse(Sum2.Text, out number2) == false) 
+            {
+                MessageBox.Show("Sum2에 문자가 들어왔습니다. 숫자를 입력해주세요 .");
+                Sum2.SelectAll();
+                Sum2.Focus();
+                return;
+            }
+            number1 = Convert.ToInt32(Sum1.Text) ;
+            number2 = Convert.ToInt32(Sum2.Text) ;
+
+            int sum = Add(number1, number2) ;
+
+            SumResult.Text = sum.ToString() ;
+        }
+
+        public int Add(int number1, int number2)
+        {
+            int sum = number1 + number2 ;
+            return sum ;
+        }
+
+        public float Add(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+
+        public int Sub(int number1, int number2)
+        {
+            int sum = number1 - number2;
+            return sum;
+        }
+
+    }
+}
