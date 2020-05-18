88d6443cc5eb0b2349499cf559008ca000214687
DESKTOP-9L4KOLL\K <kore207@naver.com>
2020-05-18 22:45:35 +0900

    no message

---
+++ C#/dotnetCore/jQueryAjaxCRUDinASPNETCore/jQueryAjaxCRUDinASPNETCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace jQueryAjaxCRUDinASPNETCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ C#/dotnetCore/jQueryAjaxCRUDinASPNETCore/jQueryAjaxCRUDinASPNETCore/Models/TransactionModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jQueryAjaxCRUDinASPNETCore.Models
+{
+    public class TransactionModel
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        public string SWIFTCode { get; set; }
+        
+        public int Amount { get; set; }
+    }
+}
