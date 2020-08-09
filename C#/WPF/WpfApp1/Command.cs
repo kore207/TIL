a7626465e8b44c0c07af950b73d89972a0f48b56
DESKTOP-9L4KOLL\K <kore207@naver.com>
2020-08-09 22:31:39 +0900

    C# WPF Binding (Command)

---
+++ C#/WPF/WpfApp1/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class CommandTemp : ICommand
+    {
+        Action<object> _executeMethod; //버튼 클릭시 수행할 action
+        Func<object, bool> _canexecuteMethod; // Action 수행전 필요한 조건을 검사하는 Func
+
+        public CommandTemp(Action<object> executeMethod, Func<object, bool> canexecuteMethod)
+        {
+            this._executeMethod = executeMethod;
+            this._canexecuteMethod = canexecuteMethod;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true; //따로 조건을 두지 않는다. 
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeMethod(parameter);
+        }
+    }
+}
---
+++ C#/WPF/WpfApp1/ViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp1
+{
+    public class ViewModel
+    {
+        public ICommand MyCommand { get; set; }
+
+        public ViewModel()
+        {
+            MyCommand = new CommandTemp(executeMethod, canexecuteMethod);
+        }
+
+        private bool canexecuteMethod(object arg)
+        {
+            return true;
+        }
+
+        private void executeMethod(object obj)
+        {
+            MessageBox.Show("코드 비하인드가 아닌 Command ExecuteMethod");
+        }
+    }
+}
